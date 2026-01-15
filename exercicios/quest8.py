316e294510bc95d7fe0be2f2ba5b61cfb16ff18c
SAVictordev <victor.alves.igarassu@gmail.com>
2026-01-15 10:52:12 -0300

    Correção da lista de exercícíos e realização da questão 8.

---
+++ exercicios/quest8.py
@@ -0,0 +1,26 @@
+## Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
+
+## Forma com metódos de array
+num1 = 0;
+num2 = 5;
+num3 = 10;
+
+vetornum = [num1, num2, num3]
+
+vetornum.sort(reverse=True);
+print(vetornum);
+
+## Forma pura com condicionais
+
+if (num1 >= num2 >= num3 ):
+    print(num1,num2,num3);
+elif(num1 >= num3 >= num2):
+    print(num1,num3,num2);
+elif(num2 >= num1 >= num3):
+    print(num2,num1,num3);
+elif(num2 >= num3 >= num1):
+    print(num2,num3,num1);
+elif(num3 >= num1 >= num2):
+    print(num3,num1,num2);
+else:
+    print(num3,num2,num1);
